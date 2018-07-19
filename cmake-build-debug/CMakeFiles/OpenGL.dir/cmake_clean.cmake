9b1a066ac8a107737dfedea8ae1629d57799f9c5
AdrianNadi <adrian.nadi1989@gmail.com>
2018-07-19 16:25:13 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL.dir/src/Application.cpp.obj"
+  "OpenGL.pdb"
+  "OpenGL.exe"
+  "OpenGL.exe.manifest"
+  "libOpenGL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
